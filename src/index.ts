a01cf521793095039c8bd1e099a47181c5b61bf5
cidaluna <cidalunna@hotmail.com>
2022-01-23 22:30:21 -0300

    some of the most common types in Typescript

--- src/index.ts
+++ src/index.ts
@@ -94,3 +94,137 @@ listaAlunos=["Maria", 40, "teste"];
 listaAlunos.push("Joao", 99, "teste jão");
 console.log(listaAlunos);
 
+// enum permite atribuir valores predefinidos, podendo ser number, string ou heterogeneous
+export enum DiaDaSemana{
+    Segunda = 1,
+    Terca = "Terça-feira",
+    Quarta = 3,
+    Quinta = 4,
+    Sexta = 5,
+    Sábado = 6,
+    Domingo = 7
+}
+
+let dia = DiaDaSemana[5];
+console.log(dia);
+let outroDia = DiaDaSemana.Terca;
+console.log(outroDia);
+
+// Union permite combinar um ou mais tipos, a mesma variável
+let minhaVar: (string | number | boolean | string[])
+minhaVar = "Cida";
+console.log(minhaVar);
+minhaVar = 99;
+console.log(minhaVar);
+minhaVar = true;
+console.log(minhaVar);
+minhaVar = ["string 1", "string 2", "string 3"];
+console.log(minhaVar);
+
+var arr: (number[] | string[]);
+var i: number;
+arr = ["teste A", "teste B", "teste C", "teste D"];
+
+for(i = 0; i < arr.length; i ++){
+    console.log(arr[i]);
+}
+
+// Union passado no parâmetro da função
+function deleteAluno(aluno: string | string[]){
+    if(typeof aluno == "string"){ // deletar um registro (typeof retorna o tipo do objeto)
+        console.log(aluno, "deletado");
+    }else{
+        var i;
+        for (i=0; i< aluno.length;i++){ // deletar mais de um rgistro
+            console.log(aluno[i], "deletado");
+        }
+    }
+}
+
+var alunos: string[] = ["Joao", "Pedro", "Jeferson", "Luana", "Priscila"];
+deleteAluno(alunos);
+
+let outraVariavel: string | number | boolean = 13;
+console.log("O tipo dessa variável é: ",typeof(outraVariavel));
+
+// Exemplo do uso de instanceOf
+interface exemploInterfaceZ{ x(): string }
+
+class A implements exemploInterfaceZ{
+    x(): string{
+        throw new Error("Method not implemented.")
+    }
+}
+
+class B implements exemploInterfaceZ{
+    x(): string{
+        throw new Error("Method not implemented.")
+    }
+}
+
+function exemploComInstanceOf(parametro: exemploInterfaceZ){
+    if(parametro instanceof A){
+        console.log("Sou da classe A.");
+    }else if(parametro instanceof B){
+        console.log("Sou da classe B.")
+    }
+}
+
+exemploComInstanceOf(new A());
+
+// Tipo any, utilizado por exemplo, quando for preciso acessar uma API de terceiros e desconhece a estrutura.
+let minhaVariavelAny: any = "Teste any";
+minhaVariavelAny = 12;
+minhaVariavelAny = true;
+console.log(minhaVariavelAny);
+
+// Tipando funções
+// função recebe 2 parametros number e retorna uma string
+function calcula(primeiroNumero: number, segundoNumero: number): string{
+    return `Resultado: ${primeiroNumero + segundoNumero}`;
+}
+
+let soma: string;
+soma = calcula(10,20);
+console.log(soma);
+
+// Void passa para o compilador que aquela funcao nao tera um retorno.
+function log(msg: string ): void {
+    console.log(`Função sem retorno: ${msg}`);
+}  
+log("Teste Console Log");
+
+// Never
+function verificandoTipo(x: string | number): boolean{
+    if(typeof x === "string"){
+        return true;
+    }else if(typeof x === "number"){
+        return false;
+    }
+    return openFail("Esse método não aceita esse tipo de type! ");
+}
+
+function openFail(message: string): never{
+    throw new Error(message);
+}
+
+console.log(verificandoTipo("Teste string"));
+console.log(verificandoTipo(199));
+
+// Type assertions funciona como o cast, é possível alterar o tipo de uma variável sem lançar Exception
+function myTypeAssertions(codigoAny: any){
+    let codigoNumber: number = <number>codigoAny;
+    return codigoNumber * 5;
+}
+console.log(myTypeAssertions(10));
+
+
+interface Student{
+    nome: string,
+    codigo: number
+}
+
+let estudante = <Student>{}
+estudante.nome = "Luna";
+estudante.codigo = 2022;
+console.log(estudante);
